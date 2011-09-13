44be907cef72e68040b35b17e5a83f0a75b70cc3
Todd Lipcon <todd@cloudera.com>
2011-09-13 16:16:16 -0700

    Add -Wall

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -fPIC -shared -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -o libmlockall_agent.so mlockall_agent.c
+gcc -Wall -fPIC -shared -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -o libmlockall_agent.so mlockall_agent.c
